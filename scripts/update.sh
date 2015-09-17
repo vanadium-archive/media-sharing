85e75ab1af9aafdfb39524f373e2e56e4279c140
Nicolas Lacasse <nlacasse@google.com>
2015-09-17 11:27:39 -0700

    TBR: media-sharing: Replace V23_ROOT with JIRI_ROOT everywhere.
    MultiPart: 16/23
    
    Change-Id: Id9b4634996e04516ce06f60bba93fa7990a14833

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,8 +7,8 @@ set -x
 set -e
 
 JIRI_PROFILE=arm v23 go install v.io/x/media_sharing/...
-mkdir -p $V23_ROOT/release/go/bin/linux_arm
-cp $V23_ROOT/release/projects/media-sharing/go/bin/linux_arm/* $V23_ROOT/release/go/bin/linux_arm
+mkdir -p $JIRI_ROOT/release/go/bin/linux_arm
+cp $JIRI_ROOT/release/projects/media-sharing/go/bin/linux_arm/* $JIRI_ROOT/release/go/bin/linux_arm
 vbecome --role=identity/role/vprod/publisher device publish --goos=linux --goarch=arm mediaserver
 
 #installation=$(device ls --installation-state=Active vlab/devices/rpi2media/devmgr/apps/mediaserver/* | tail -n 1)
